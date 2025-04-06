f2050cc03c032e71326732422f7d12070f6def49
Maks_Hofman <makser.hofman@gmail.com>
2025-04-06 21:46:51 +0200

    to do list

--- exp1/TOP_GA.py
+++ exp1/TOP_GA.py
@@ -1,7 +1,14 @@
 from rover import Rover
 from poi import Position, Poi
 from swarm import Swarm
-import random
+import random, time
+
+"""
+To do:
+Poprawic selekcje osobnikow(zrobic na te 2 standardowe sposoby)
+Napisac sensowne experymenty
+"""
+
 
 def TOP_GA_TRAIN(amount_of_pois, amount_of_rovers, population_size):
     Amout_of_pois = amount_of_pois
@@ -22,7 +29,6 @@ def TOP_GA_TRAIN(amount_of_pois, amount_of_rovers, population_size):
     for _ in range(POPULATION_SIZE): 
                 chrom = Swarm.create_gnome(poi_list, amount_of_rovers, Position(0,0)) 
                 population.append(Swarm(Position(0,0),chrom, poi_list, amount_of_rovers)) 
-    print("wygenerowala sie startowa populacja")
     while not loop_after_peak == 0: 
         population = sorted(population, key=lambda x: x.fitness, reverse=True)
         if population[0].fitness >= best_fitness:  
@@ -57,13 +63,34 @@ def TOP_GA_TRAIN(amount_of_pois, amount_of_rovers, population_size):
         population = new_generation 
   
         #print(f"Generation: {generation} String: {population[0].chromosome} Fitness: {population[0].fitness}") # trzeba to na wizualizacje zmienic
-        print(f"Best fitness = {best_fitness}, Last fitness = {Last_best_fitness}, Geratiuon {generation}")
+        #print(f"Best fitness = {best_fitness}, Last fitness = {Last_best_fitness}, Geratiuon {generation}")
       
         generation += 1
 
 def exp1():
-    pass
+    start_time_50poi = time.time()
+    TOP_GA_TRAIN(50,5,1000)
+    end_time50poi = time.time()
+    deltaTime50poi = end_time50poi - start_time_50poi
+
+    start_time_100poi = time.time()
+    TOP_GA_TRAIN(100,10,1000)
+    end_time100poi = time.time()
+    deltaTime100poi = end_time100poi - start_time_100poi
+
+    start_time_200poi = time.time()
+    TOP_GA_TRAIN(200,20,1000)
+    end_time200poi = time.time()
+    deltaTime200poi = end_time200poi - start_time_200poi
+
+    start_time_500poi = time.time()
+    TOP_GA_TRAIN(500,50,1000)
+    end_time500poi = time.time()
+    deltaTime500poi = end_time500poi - start_time_500poi
+
+    print(f"Dla 50 = {deltaTime50poi},Dla 100 = {deltaTime100poi},Dla 200 = {deltaTime200poi},Dla 500 = {deltaTime500poi}")
 
 if __name__ == "__main__":
     print("start")
-    TOP_GA_TRAIN(50,5,500)
+    #TOP_GA_TRAIN(50,5,500)
+    exp1()
